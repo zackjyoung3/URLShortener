07ca65c1a061f5378438f8306d8518554a65e4c4
zackjyoung3 <70529224+zackjyoung3@users.noreply.github.com>
2020-10-26 19:50:56 -0400

    Update UserController.cs

--- URLShortener.Services/Controllers/UserController.cs
+++ URLShortener.Services/Controllers/UserController.cs
@@ -5,21 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using URLShortener.Services.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace URLShortener.Services.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        /*
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
-        */
         [HttpGet]
         public ActionResult<User> Get(string userName, string passWord)
         {
@@ -27,29 +18,11 @@ namespace URLShortener.Services.Controllers
             
             //Create the database context
             MyContext context = new MyContext();
-
-            //Now get the user of the context
-            //var user = context.Users.FirstOrDefault(row => row.ID == id);
-
+            
+            //retrieving the user from the database whose name matches the given username and password
             var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
 
-            //return the user info
-            return user;
-        }
-
-        [HttpGet]
-        [Route("User/Login")]
-        public ActionResult<User> Login(string userName, string passWord)
-        {
-            //Todo - Need to use dependency injection to create the context
-
-            //Create the database context
-            MyContext context = new MyContext();
-
-            //Now get the user of the context
-            var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
-
-            //return the user info
+            //return the user 
             return user;
         }
     }
