583c0ba96ae888746ae20cfdb6135cb96399286f
zackjyoung3 <70529224+zackjyoung3@users.noreply.github.com>
2020-10-26 19:45:43 -0400

    Update ShortenerController.cs

--- URLShortener.Services/Controllers/ShortenerController.cs
+++ URLShortener.Services/Controllers/ShortenerController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using URLShortener.Services.Models;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace URLShortener.Services.Controllers
 {
@@ -20,71 +19,20 @@ namespace URLShortener.Services.Controllers
 
             //Create the database context
             MyContext context = new MyContext();
-
-            //Now get the user of the context
-            //var user = context.Users.FirstOrDefault(row => row.ID == id);
-
-            //var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
+            
+            //creating an instance of a URL and then populating it with the longurl and shortened url input
+            //then writing it to the db
             URL anotherURL = new URL();
             anotherURL.LongURL = longurl;
             anotherURL.ShortenedURL = shorturl;
             context.URL.Add(anotherURL);
-            //URL boss = new URL();
-            //return the user info
 
+            //saving the changes
             context.SaveChanges();
 
             return anotherURL;
         }
-        /*
-        [HttpGet]
-        public ActionResult<URL> Get(string ID, string shortened)
-        {
-                //Create the database context
-                MyContext context = new MyContext();
-
-                //var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
-                var url = context.URL.FirstOrDefault(row => row.User.ID.ToString() == ID && row.ShortenedURL == shortened);
-                //URL boss = new URL();
-                //return the user info
-                return url;
-        }
-        */
-        /*
-        [HttpGet]
-        public ActionResult<URL> Get(string shortened)
-        {
-            //Todo - Need to use dependency injection to create the context
-            int UserID = Int32.Parse(ID);
-            //Create the database context
-            MyContext context = new MyContext();
-
-            //Now get the user of the context
-            //var user = context.Users.FirstOrDefault(row => row.ID == id);
-
-            //var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
-            var urls = context.URL.Where(row => row.User.ID == UserID).ToList();
-            //URL boss = new URL();
-            //return the user info
-            return urls;
-        }
-        
-        [HttpGet]
-        [Route("User/Login")]
-        public ActionResult<User> Login(string userName, string passWord)
-        {
-            //Todo - Need to use dependency injection to create the context
-
-            //Create the database context
-            MyContext context = new MyContext();
-
-            //Now get the user of the context
-            var user = context.Users.FirstOrDefault(row => row.UserName == userName && row.PassWord == passWord);
-
-            //return the user info
-            return user;
-        }
-        */
+       
     }
 }
 
